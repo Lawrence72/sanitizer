06829d8eac3f6805bb9e559bad3e6222e700e83d
Lawrence <lawrence.72@yahoo.com>
2024-02-27 07:47:07 -0600

    add encoding

--- tests/StringTest.php
+++ tests/StringTest.php
@@ -40,4 +40,14 @@ final class StringTest extends TestCase {
 		$text = "<b>hello world!@#$%^&*()_+</b>";
 		$this->assertSame("<b>hello world!@#$%^&amp;*()_+</b>", $this->sanitizer->clean($text, ['b']));
 	}
+
+	public function testEncoding() {
+		$text = "Привет! Hello! ¡Hola! 你好! مرحبا! Bonjour! 안녕하세요! Ciao! Olá!";
+		$this->assertSame("&#1055;&#1088;&#1080;&#1074;&#1077;&#1090;! Hello! &iexcl;Hola! &#20320;&#22909;! &#1605;&#1585;&#1581;&#1576;&#1575;! Bonjour! &#50504;&#45397;&#54616;&#49464;&#50836;! Ciao! Ol&aacute;!", $this->sanitizer->clean($text));
+	}
+
+	public function testEncodingWithTags() {
+		$text = "<b>Привет! Hello! ¡Hola! 你好! مرحبا! Bonjour! 안녕하세요! Ciao! Olá!</b>";
+		$this->assertSame("<b>&#1055;&#1088;&#1080;&#1074;&#1077;&#1090;! Hello! &iexcl;Hola! &#20320;&#22909;! &#1605;&#1585;&#1581;&#1576;&#1575;! Bonjour! &#50504;&#45397;&#54616;&#49464;&#50836;! Ciao! Ol&aacute;!</b>", $this->sanitizer->clean($text, ['b']));
+	}
 }
